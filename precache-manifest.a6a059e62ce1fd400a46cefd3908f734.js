496e19e58677c89022a5f68a2813398211f65d2c
Thomas Cheng <thomascheng81@gmail.com>
2019-04-19 15:48:27 -0400

    Updates

---
+++ precache-manifest.a6a059e62ce1fd400a46cefd3908f734.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ccdad0e3360744ce9c5f",
+    "url": "/allocation-reports/static/js/runtime~main.0f3dbf7f.js"
+  },
+  {
+    "revision": "0e7d96b4433225aadbe5",
+    "url": "/allocation-reports/static/js/main.bd208906.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/allocation-reports/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "0e7d96b4433225aadbe5",
+    "url": "/allocation-reports/static/css/main.914c3ce7.chunk.css"
+  },
+  {
+    "revision": "4d1c132868b7c8d80bf39abbec418f7d",
+    "url": "/allocation-reports/index.html"
+  }
+];
