4d9c46135de5d3622dfb68572735080d83b8beb7
Thomas Cheng <thomascheng81@gmail.com>
2019-04-19 16:31:41 -0400

    Fetch accounts

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
+import { getAuthorizationToken } from "./authorization";
 
 const CLIENT_ID = "YCUSnajluQMAHR32DnJhupUYJddjZQ";
 const REDIRECT_URI = "https://tomcheng.github.io/allocation-reports";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <a
-          href={`https://login.questrade.com/oauth2/authorize?client_id=${CLIENT_ID}&response_type=token&redirect_uri=${REDIRECT_URI}`}
-        >
-          Authorize
-        </a>
-      </div>
-    );
-  }
-}
+const { accessToken, apiServer } = getAuthorizationToken();
+
+const App = () => {
+  useEffect(() => {
+    console.log("make api call here.");
+    axios.get(`${apiServer}v1/accounts`, {
+      headers: { Authorization: `Bearer ${accessToken}` }
+    });
+  }, []);
+  return (
+    <div>
+      <a
+        href={`https://login.questrade.com/oauth2/authorize?client_id=${CLIENT_ID}&response_type=token&redirect_uri=${REDIRECT_URI}`}
+      >
+        Authorize
+      </a>
+    </div>
+  );
+};
 
 export default App;
