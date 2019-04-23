fe51b24ae3162ba9b77664dbdbedecdcf5ceb639
Thomas Cheng <thomascheng81@gmail.com>
2019-04-22 23:47:01 -0400

    Make accounts collapsible

--- src/Account.js
+++ src/Account.js
@@ -1,16 +1,29 @@
 import React from "react";
 import styled from "styled-components";
 import sumBy from "lodash/sumBy";
+import AnimateHeight from "react-animate-height-auto";
 import { BREAKDOWNS } from "./constants";
 import { Subheading } from "./styleComponents";
 import LineItem from "./LineItem";
+import { formatMoney } from "./utils";
+import { useLocalStorage } from "./hooks";
 
 const Heading = styled.h2`
-  margin-top: 30px;
-  margin-bottom: 10px;
+  display: flex;
+  justify-content: space-between;
+  margin-top: 40px;
+  border-bottom: 2px solid #333;
+  font-size: 18px;
+  line-height: 24px;
+  padding-bottom: 5px;
+  margin-bottom: 0;
 `;
 
 const Account = ({ account, balance, positions, postTaxAdjustment }) => {
+  const [isExpanded, setIsExpanded] = useLocalStorage(
+    `__port-is-expanded-${account.number}__`,
+    true
+  );
   const stocks =
     sumBy(
       positions,
@@ -28,20 +41,29 @@ const Account = ({ account, balance, positions, postTaxAdjustment }) => {
 
   return (
     <div key={account.number}>
-      <Heading>{account.type}</Heading>
-      <Subheading>Asset Classes</Subheading>
-      <LineItem label="Stocks" amount={stocks} total={total} />
-      <LineItem label="Bonds" amount={bonds} total={total} />
-      <LineItem label="Cash" amount={cash} total={total} />
-      <Subheading>Positions</Subheading>
-      {positions.map(position => (
-        <LineItem
-          key={position.symbol}
-          label={position.symbol}
-          amount={position.currentMarketValue * postTaxAdjustment}
-          total={total - cash}
-        />
-      ))}
+      <Heading
+        onClick={() => {
+          setIsExpanded(!isExpanded);
+        }}
+      >
+        <span>{account.type}</span>
+        <span>{formatMoney(total)}</span>
+      </Heading>
+      <AnimateHeight isExpanded={isExpanded}>
+        <Subheading>Asset Classes</Subheading>
+        <LineItem label="Stocks" amount={stocks} total={total} />
+        <LineItem label="Bonds" amount={bonds} total={total} />
+        <LineItem label="Cash" amount={cash} total={total} />
+        <Subheading>Positions</Subheading>
+        {positions.map(position => (
+          <LineItem
+            key={position.symbol}
+            label={position.symbol}
+            amount={position.currentMarketValue * postTaxAdjustment}
+            total={total - cash}
+          />
+        ))}
+      </AnimateHeight>
     </div>
   );
 };
